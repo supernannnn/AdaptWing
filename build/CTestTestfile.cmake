cd47bfc02e46ba6779c647275a50a66b920cddfa
supernannnn <1792977561@qq.com>
2023-06-15 23:49:37 +0800

    add README

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,9 +5,18 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("realsense-ros/realsense2_description")
 subdirs("location/VINS-Fusion/camera_models")
 subdirs("location/VINS-Fusion/global_fusion")
 subdirs("location/VINS-Fusion/loop_fusion")
+subdirs("planner/EGO-Planner/plan_env")
+subdirs("planner/EGO-Planner/path_searching")
+subdirs("planner/EGO-Planner/drone_detect")
 subdirs("location/rtk")
+subdirs("realsense-ros/realsense2_camera")
+subdirs("planner/EGO-Planner/traj_utils")
+subdirs("planner/EGO-Planner/bspline_opt")
+subdirs("planner/EGO-Planner/plan_manage")
+subdirs("planner/EGO-Planner/rosmsg_tcp_bridge")
 subdirs("control/px4ctrl")
 subdirs("location/VINS-Fusion/vins_estimator")
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -11,5 +11,6 @@ export CMAKE_PREFIX_PATH="/home/skbt/AdaptWing/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/skbt/AdaptWing/devel/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/home/skbt/AdaptWing/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/skbt/AdaptWing/build'
+export PYTHONPATH="/home/skbt/AdaptWing/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/skbt/AdaptWing/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/skbt/AdaptWing/src:$ROS_PACKAGE_PATH"
